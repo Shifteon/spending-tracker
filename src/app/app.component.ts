4f9b0132c23788de75ee7ed4801eb286fa808cb8
Benjamin Wyatt <bpwyatt04@gmail.com>
2024-11-09 12:02:59 -0800

    Test Vercel function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api/api.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,14 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'spending-tracker';
+
+  constructor(private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    this.apiService.getTestMessage().subscribe(data => {
+      console.log(data);
+    });
+  }
 }
